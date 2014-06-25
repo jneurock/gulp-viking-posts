9f8f4613d49fb29e74a5bb9957f4e05c71d0008d
Rocky Neurock <jneurock@gmail.com>
2014-06-24 22:31:05 -0700

    First pass at this gulp plugin

--- README.md
+++ README.md
@@ -1,4 +1,149 @@
-gulp-viking-posts
-=================
+Viking Posts is a [gulp](http://gulpjs.com/) plugin meant to work with [Viking Base](https://github.com/jneurock/viking-base) to create lightweight JSON blog posts from Markdown files. The plugin can be used without Viking Base, too.
 
-A gulp plugin for generating lightweight JSON blog posts. Meant for use with Viking Base but can be used standalone.
+## Getting Started
+
+Install the package via `npm`:
+
+`npm install --save-dev gulp-viking-posts`
+
+Edit the gulp file in your Viking Base project:
+```javascript
+// Output updates
+vb.output.post = 'posts/';
+vb.output.posts = 'posts.json';
+
+// Source updates
+vb.sources.posts = 'posts/**/*.md';
+
+// Task updates
+vb.tasks.build.depends = [
+  'handlebars',
+  'js-doc',
+  'posts',
+  'root'
+];
+
+vb.tasks.posts = {
+  cb: function() {
+
+    return gulp.src( this.sources.posts )
+      .pipe( plugins.vikingPosts() )
+      .pipe( gulp.dest( this.output.publish + this.output.post ) )
+      .pipe( plugins.concat( this.output.posts, {
+        newLine: ','
+      }))
+      .pipe( plugins.vikingPosts({
+        concat: true
+      }))
+      .pipe( gulp.dest( this.output.publish + this.output.post ) );
+  }
+};
+
+```
+
+In the above example, 4 key steps are shown:
+
+1. Post output is specified. There's a path to the source posts, `vb.output.post`, and a file name for concatenated posts `vb.output.posts`.
+2. Post source is defined `vb.sources.posts`.
+3. Build dependencies now include posts task.
+4. Posts task is defined. The example above also shows how to concatenate posts into one file.
+
+Note: Concatenating posts is optional and is easily done by using the `gulp-concat` plugin that Viking Base already depends on. If you aren't using Viking Base, you'll need to include `gulp-concat` yourself. Pay close attention to the second argument passed to `plugins.concat`. This is an options hash for `gulp-concat`. It's critical that the `newLine` property's value is `','`. If not, `gulp-concat` will separate posts with a new line and produce invalid JSON for the final output. It's also key to pass an options hash to the second occurence of `plugins.vikingPosts` with a `concat` property having a value of `true`. This tells the Viking Posts plugin to work with concatenated posts and to remove the full content from the concatenated posts, leaving only the excerpts.
+
+## Using gulp-viking-posts without Viking Base
+
+Using this plugin without Viking Base is perfectly fine but the above example won't be too helpful.
+
+A more abstract example:
+```javascript
+var concat = require('gulp-concat'),
+    gulp = require('gulp'),
+    vp = require('gulp-viking-posts');
+
+gulp.task('posts', function() {
+  
+  return gulp.src('posts/**/*.md')
+    .pipe( vp() )
+    .pipe( gulp.dest('dist/posts') )
+    .pipe( concat( 'posts.json', {
+      newLine: ','
+    }))
+    .pipe( vp({
+      concat: true
+    }))
+    .pipe( gulp.dest('dist/posts') );
+});
+```
+
+## Writing Posts
+
+Posts are simply [Markdown](http://daringfireball.net/projects/markdown/) files that can optionally include [YAML front matter](http://assemble.io/docs/YAML-front-matter.html). If front matter is ommitted, Viking Posts will get the title from the file name, the excerpt from the first `<p>` tag it finds, the creation and updated dates from the file system and the category from the file path. For example, a file found in `posts/articles/` that has no front matter will be categorized in "articles".
+
+Example:
+```markdown
+---
+title: Hello World
+category: articles
+excerpt: My first post!
+created: 6/24/2014
+updated:
+---
+
+# Hello World
+
+This is my first post!
+```
+
+## Custom Front Matter
+
+Any front matter that isn't `title`, `category`, `content`, `created`, `excerpt` or `updated` will be added to the generated post object.
+
+## Plugin Options
+
+You may pass an options hash to the Viking Posts plugin. The options are:
+
+* `concat: {boolean}` Tells the Viking Posts plugin to concatenate posts
+* `formatDate: {function}` User-defined function that overrides the build in date formatting function. Expects a date or a string to be passed in.
+* `sortPosts: {function}` User-defined sort function for concatenated posts. Defaults to date descending, category, title. Expects two post objects to be passed in.
+* `titleSeparator: {string}` For use without front matter. Viking Posts will generate a post title from the file name, separating words by this separator. Default is `'-'`.
+
+## Example Output
+
+Single post `dist/posts/articles/hello-world.json`:
+```json
+{
+  "title": "Hello World",
+  "category": "articles",
+  "excerpt": "<p>My firt post</p>",
+  "content": "<h1>Hello World</h1>\n<p>My first post is here!</p>",
+  "created": "6/20/2014",
+  "updated": ""
+}
+```
+
+Concatenated posts `dist/posts/posts.json`:
+```json
+[
+  {
+    "title": "Chicken Soup",
+    "category": "recipes",
+    "excerpt": "<p>A delicious chicken soup recipe that's been in my family for generations.</p>",
+    "created": "6/19/2014",
+    "updated": "6/24/2014"
+  },
+  {
+    "title": "Hello World",
+    "category": "articles",
+    "excerpt": "<p>My firt post</p>",
+    "created": "6/20/2014",
+    "updated": ""
+  }
+]
+```
+
+## References
+* [Viking Base](https://github.com/jneurock/viking-base)
+* [npm](http://npmjs.org/)
+* [gulp](http://gulpjs.com/)
+* [Markdown](http://daringfireball.net/projects/markdown/)
+* [YAML front matter](http://assemble.io/docs/YAML-front-matter.html)
---
+++ index.js
@@ -0,0 +1,387 @@
+var DOMParser = require('xmldom').DOMParser,
+		frontMatter = require('front-matter'),
+		gulpUtil = require('gulp-util'),
+		markdown = require('markdown').markdown,
+		through = require('through2'),
+		titleCase = require('to-title-case'),
+		titleSeparator = '-';
+
+/**
+ * Create a post object
+ *
+ * @param {Object} file
+ * @param {Object} [options]
+ * @returns {string} post object as a string
+ */
+function createPost( file, options ) {
+
+	var _formatDate = options && typeof options.formatDate === 'function' ?
+				options.formatDate : formatDate,
+			post = new Post();
+
+	// Parse front matter to get as many post attributes as possible
+	parseFrontMatter( file, post, options );
+
+	// Get category from path if not from front matter
+	if ( !post.category ) {
+
+		post.category = getCategoryFromPath( file );
+	}
+
+	// Get creation date from file if not from front matter
+	if ( !post.created ) {
+
+		post.created = _formatDate( file.stat.ctime );
+	}
+
+	// Get excerpt from first <p> tag if not from front matter
+	if ( !post.excerpt ) {
+
+		post.excerpt = getDefaultExcerpt( post.content );
+	}
+
+	// Get title from file name if not from front matter
+	if ( !post.title ) {
+
+		post.title = getTitleFromPath( file );
+	}
+
+	// Get updated date from file if not from front matter
+	if ( !post.updated ) {
+
+		post.updated = _formatDate( file.stat.mtime );
+	}
+
+	return JSON.stringify( post );
+}
+
+/**
+ * Format a date
+ *
+ * @param {(Object|string)} date The date object to format
+ * @returns {string}
+ */
+function formatDate( date ) {
+
+	var dateString = '';
+
+	date = new Date( date );
+
+	dateString = date.getMonth() + 1;
+	dateString += '/';
+	dateString += date.getDate();
+	dateString += '/';
+	dateString += date.getFullYear();
+
+	return dateString;
+}
+
+/**
+ * Get post category from file path
+ *
+ * @param {Object} file
+ * @returns {string}
+ */
+function getCategoryFromPath( file ) {
+
+	var category = '',
+			i = 0,
+			parts = file.path.split('/'),
+			len = parts.length,
+			last = len - 1;
+
+	for ( ; i < len; i++ ) {
+
+		/*
+		 * If previous path part is "posts" and the
+		 * current path part isn't the last path part
+		 */
+		if ( parts[i - 1] === 'posts' && i !== last ) {
+
+			category = parts[i];
+
+			break;
+		}
+	}
+
+	return category;
+}
+
+/**
+ * Get the post excerpt from the first <p> tag found
+ *
+ * @param {string} content The full post content
+ * @returns {string}
+ */
+function getDefaultExcerpt( content ) {
+
+	var doc = new DOMParser().parseFromString( content, 'text/html' ),
+			pTags = doc.getElementsByTagName('p');
+
+	if ( pTags.length ) {
+
+		return '<p>' + pTags[0].firstChild.nodeValue + '</p>';
+
+	} else {
+
+		return '';
+	}
+}
+
+/**
+ * Get post title from file path
+ *
+ * @param {Object} file
+ * @param {Object} [options]
+ * @returns {string}
+ */
+function getTitleFromPath( file, options ) {
+
+	var _titleSeparator = options && options.titleSeparator ?
+				options.titleSeparator : titleSeparator,
+			i = 0,
+			parts = file.path.split('/'),
+			fileParts = parts[parts.length - 1].split('.'),
+			filename = fileParts[0],
+			fileNameParts = filename.split( _titleSeparator ),
+			len = fileNameParts.length,
+			title = '';
+
+	for ( ; i < len; i++ ) {
+
+		title += i ? ' ' : '';
+		title += fileNameParts[i];
+	}
+
+	return titleCase( title );
+}
+
+/**
+ * Check if front matter attribute is custom or not
+ *
+ * @param {string} attr The attribute name
+ * @returns {boolean}
+ */
+function isCustomFrontMatter( attr ) {
+
+	if ( attr !== 'title' && attr !== 'category' && attr !== 'content' &&
+		attr !== 'created' && attr !== 'excerpt' && attr !== 'updated' ) {
+
+		return true;
+
+	} else {
+
+		return false;
+	}
+}
+
+/**
+ * Parse YAML front matter from post
+ *
+ * @param {Object} file The post file object
+ * @param {Object} post The new post object we're working with
+ * @param {Object} [options]
+ */
+function parseFrontMatter( file, post, options ) {
+
+	var _formatDate = options && typeof options.formatDate === 'function' ?
+				options.formatDate : formatDate,
+			content = frontMatter( file.contents.toString() ),
+			prop = '';
+
+	if ( content.attributes ) {
+
+		post.category = content.attributes.category || '';
+		post.created = content.attributes.created ? _formatDate( content.attributes.created ) : '';
+		post.title = content.attributes.title || '';
+		post.updated = content.attributes.updated ? _formatDate( content.attributes.updated ) : '';
+
+		if ( content.attributes.excerpt ) {
+
+			post.excerpt = markdown.toHTML( content.attributes.excerpt );
+		}
+
+		post.content = content.body ? markdown.toHTML( content.body ) : '';
+
+		// Look for custom front-matter
+		for ( prop in content.attributes ) {
+
+			if ( content.attributes.hasOwnProperty(prop) && isCustomFrontMatter( prop ) ) {
+
+				post[ prop ] = content.attributes[ prop ];
+			}
+		}
+	}
+}
+
+/**
+ * Post object constructor
+ *
+ * @constructor
+ */
+function Post() {
+
+	return {
+		/**
+		 * Optional post category
+		 *
+		 * @type {string}
+		 */
+		category: '',
+		/**
+		 * Full post content
+		 *
+		 * @type {string}
+		 */
+		content: '',
+		/**
+		 * Post creation date
+		 *
+		 * @type {string}
+		 */
+		created: '',
+		/**
+		 * Optional post excerpt
+		 *
+		 * @type {string}
+		 */
+		excerpt: '',
+		/**
+		 * Post title
+		 *
+		 * @type {string}
+		 */
+		title: '',
+		/**
+		 * Optional post updated date
+		 *
+		 * @type {string}
+		 */
+		updated: ''
+	};
+}
+
+/**
+ * Sort posts. By default posts are sorted by
+ * date descending, category, title
+ *
+ * @param {Object} a
+ * @param {Object} b
+ */
+function sortPosts( a, b ) {
+
+	var dateA = a.updated || a.created,
+			dateB = b.updated || b.created;
+
+	// Convert dates
+	dateA = new Date( dateA );
+	dateB = new Date( dateB );
+
+	// See if dates match
+	if ( dateB - dateA === 0 ) {
+
+		// See if categories are the same
+		if ( a.category === b.category ) {
+
+			// Sort by title
+			return a.title.localeCompare( b.title );
+
+		// Sort by category
+		} else {
+
+			return a.category.localeCompare( b.category );
+		}
+
+	// Sort by date descending
+	} else {
+
+		return dateB - dateA;
+	}
+}
+
+/**
+ * Iterate over posts objects and delete the content property
+ *
+ * @param {string} posts
+ * @param {Object} [options]
+ * @returns {string}
+ */
+function removePostsContent( posts, options ) {
+
+	var _sortPosts = options && typeof options.sortPosts === 'function' ?
+				options.sortPosts : sortPosts,
+			i = 0,
+			len = 0;
+
+	// Posts should be objects separated by commas but not wrapped in []
+	posts = JSON.parse( '[' + posts.contents.toString() + ']' );
+
+	len = posts.length;
+
+	for ( i; i < len; i++ ) {
+
+		delete posts[i].content;
+	}
+
+	// Sort posts by newest
+	posts.sort( _sortPosts );
+
+	return JSON.stringify( posts );
+}
+
+module.exports = function( options ) {
+
+	options = options || {};
+
+	return through.obj(function( file, enc, cb ) {
+
+		if ( file.isNull() ) {
+
+			this.push( file );
+
+			return cb();
+		}
+
+		if ( file.isStream() ) {
+
+			this.emit('error', new gulpUtil.PluginError('gulp-viking-blog', 'Streaming not supported'));
+
+			return cb();
+		}
+
+		// Override format date function
+		if ( options.formatDate && typeof options.formatDate === 'function' ) {
+
+			formatDate = options.formatDate;
+		}
+
+		// Override sort posts function
+		if ( options.sortPosts && typeof options.sortPosts === 'function' ) {
+
+			sortPosts = options.sortPosts;
+		}
+
+		// Override title separator for use with generating title from file name
+		if ( options.titleSeparator ) {
+
+			titleSeparator = options.titleSeparator;
+		}
+
+		// If concat option is set, iterate over post objects and remove full content
+		if ( options.concat ) {
+
+			file.contents = new Buffer( removePostsContent( file, options ) );
+
+		// Else create post object
+		} else {
+
+			file.contents = new Buffer( createPost( file, options ) );
+
+			file.path = gulpUtil.replaceExtension( file.path, '.json' );
+		}
+
+		this.push( file );
+
+		cb();
+	});
+};
